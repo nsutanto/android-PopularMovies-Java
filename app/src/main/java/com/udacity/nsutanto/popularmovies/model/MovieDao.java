b8621484913e68afbdca5b930aebf4aa3e229f81
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-08-15 07:18:37 -0500

    feat: Add live data

--- app/src/main/java/com/udacity/nsutanto/popularmovies/model/MovieDao.java
+++ app/src/main/java/com/udacity/nsutanto/popularmovies/model/MovieDao.java
@@ -1,19 +1,19 @@
 package com.udacity.nsutanto.popularmovies.model;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
-
 import java.util.List;
 
 @Dao
 public interface MovieDao {
 
     @Query("SELECT * FROM Movie")
-    List<Movie>  loadAllMovies();
+    LiveData<List<Movie>> loadAllMovies();
 
     @Insert
     void insertMovie(Movie movie);
