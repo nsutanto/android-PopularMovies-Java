a080ec2f3fca9462081a2206f70ce564e5c0da44
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-08-16 08:56:13 -0500

    feat: Implement Trailer

--- app/src/main/java/com/udacity/nsutanto/popularmovies/DetailActivity.java
+++ app/src/main/java/com/udacity/nsutanto/popularmovies/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,17 +16,21 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.nsutanto.popularmovies.adapter.ReviewAdapter;
+import com.udacity.nsutanto.popularmovies.adapter.VideoAdapter;
 import com.udacity.nsutanto.popularmovies.listener.ITaskReviewListener;
+import com.udacity.nsutanto.popularmovies.listener.ITaskVideoListener;
 import com.udacity.nsutanto.popularmovies.model.AppDatabase;
 import com.udacity.nsutanto.popularmovies.model.Movie;
 import com.udacity.nsutanto.popularmovies.model.Review;
+import com.udacity.nsutanto.popularmovies.model.Video;
 import com.udacity.nsutanto.popularmovies.task.FetchReviewTask;
+import com.udacity.nsutanto.popularmovies.task.FetchVideoTask;
 import com.udacity.nsutanto.popularmovies.utils.NetworkUtils;
 
 import java.net.URL;
 import java.util.ArrayList;
 
-public class DetailActivity extends AppCompatActivity implements ITaskReviewListener {
+public class DetailActivity extends AppCompatActivity implements ITaskReviewListener, ITaskVideoListener {
 
     private TextView mTitle;
     private ImageView mImageView;
@@ -35,8 +40,11 @@ public class DetailActivity extends AppCompatActivity implements ITaskReviewList
     private Button mFavorite;
     private AppDatabase mAppDatabase;
     private RecyclerView mRVReview;
+    private RecyclerView mRVTrailer;
     private ReviewAdapter mReviewAdapter;
+    private VideoAdapter mVideoAdapter;
     private LinearLayoutManager mReviewLayoutManager;
+    private LinearLayoutManager mTrailersLayoutManager;
     private Movie mMovie;
 
     @Override
@@ -52,6 +60,7 @@ public class DetailActivity extends AppCompatActivity implements ITaskReviewList
 
         initUI();
         initReviewRecyclerView();
+        initTrailersRecyclerView();
 
         mTitle.setText(mMovie.getTitle());
 
@@ -81,6 +90,7 @@ public class DetailActivity extends AppCompatActivity implements ITaskReviewList
         }
 
         loadReviews();
+        loadTrailers();
     }
 
     private void initUI() {
@@ -90,6 +100,8 @@ public class DetailActivity extends AppCompatActivity implements ITaskReviewList
         mVoteAverage = findViewById(R.id.tv_vote);
         mOverview = findViewById(R.id.tv_overview);
         mFavorite = findViewById(R.id.buttonFavorite);
+        mRVReview = findViewById(R.id.rv_review);
+        mRVTrailer = findViewById(R.id.rv_trailer);
     }
 
     public URL GetReviewURL() {
@@ -97,9 +109,28 @@ public class DetailActivity extends AppCompatActivity implements ITaskReviewList
         return reviewURL;
     }
 
-    public void OnPostExecute(ArrayList<Review> reviews) {
+    public URL GetVideoURL() {
+        URL videoURL = NetworkUtils.BuildVideosURL(String.valueOf(mMovie.getId()));
+        return videoURL;
+    }
+
+    public void OnPostExecuteReviewTask(ArrayList<Review> reviews) {
         if (!reviews.isEmpty()) {
             mReviewAdapter.setReviews(reviews);
+        }
+    }
+
+    public void OnPostExecuteVideoTask(ArrayList<Video> videos) {
+        if (!videos.isEmpty()) {
+            mVideoAdapter.setVideos(videos);
+        }
+    }
+
+    public void PlayVideo(Video video) {
+        String url = video.getYouTubeURL();
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
         }
     }
 
@@ -129,7 +160,6 @@ public class DetailActivity extends AppCompatActivity implements ITaskReviewList
     }
 
     private void initReviewRecyclerView() {
-        mRVReview = findViewById(R.id.rv_review);
 
         mReviewLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mReviewLayoutManager.setSmoothScrollbarEnabled(true);
@@ -138,7 +168,17 @@ public class DetailActivity extends AppCompatActivity implements ITaskReviewList
         mRVReview.setHasFixedSize(true);
         mReviewAdapter = new ReviewAdapter();
         mRVReview.setAdapter(mReviewAdapter);
+    }
 
+    private void initTrailersRecyclerView() {
+
+        mTrailersLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRVTrailer.setLayoutManager(mTrailersLayoutManager);
+        mRVTrailer.setHasFixedSize(true);
+        mRVTrailer.setNestedScrollingEnabled(false);
+
+        mVideoAdapter = new VideoAdapter(this);
+        mRVTrailer.setAdapter(mVideoAdapter);
     }
 
     private void loadReviews() {
@@ -148,6 +188,18 @@ public class DetailActivity extends AppCompatActivity implements ITaskReviewList
 
         if (info != null && info.isConnectedOrConnecting()) {
             new FetchReviewTask().execute(this);
+        } else {
+
+        }
+    }
+
+    private void loadTrailers() {
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+
+        if (info != null && info.isConnectedOrConnecting()) {
+            new FetchVideoTask().execute(this);
         } else {
 
         }
